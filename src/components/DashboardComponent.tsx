d3c034eaa5c2746f4c715203c589df526e780aca
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-03-24 16:23:16 -0700

    im so done with this man...

--- src/components/DashboardComponent.tsx
+++ src/components/DashboardComponent.tsx
@@ -27,7 +27,7 @@ const DashboardComponent = () => {
   //     toggle: {
   //       checked: {
   //         color: {
-  //           primary: "bg-gradient-to-r from-blue-500 to-green-400",
+  //           primary: "bg-gradient-to-r from-[#378fe6] to-[#3eda82]",
   //         },
   //       },
   //     },
@@ -57,14 +57,14 @@ const DashboardComponent = () => {
   return (
     // <Flowbite theme={{ theme: customTheme }}>
     <Flowbite>
-      <div className="text-sm grid gap-7 lg:grid-cols-4 grid-cols-1 bg-white dark:bg-neutral-800 text-gray-500 dark:text-slate-400 h-screen p-10 lg:px-32">
+      <div className="text-sm grid gap-7 lg:grid-cols-4 grid-cols-1 bg-white dark:bg-[#1e202a] text-[#63687e] dark:text-[#8b97c6] h-screen p-10 lg:px-32">
         <div className="lg:col-span-3">
-          <h2 className="text-2xl text-neutral-800 dark:text-white">
+          <h2 className="text-2xl text-[#1e202a] dark:text-white">
             Social Media Dashboard
           </h2>
           <p className="text-sm">Total Followers: 23,004</p>
         </div>
-        <hr className="lg:hidden border-gray-500 dark:border-slate-400" />
+        <hr className="lg:hidden border-[#63687e] dark:border-[#8b97c6]" />
         <div className="inline-flex items-center cursor-pointer justify-end">
           {/* Side Note: Tried to experiment with an imported <ToggleSwitch /> component, managed to get custom colors after some messing around, however colors could not match image. Despite this, it is noteworthy and could be used for future reference. Uncomment the next line to see for yourself */}
           {/* <ToggleSwitch
@@ -86,20 +86,20 @@ const DashboardComponent = () => {
               className="sr-only peer"
               onClick={() => handleDark()}
             />
-            {/* <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer bg-gradient-to-r from-blue-500 to-green-400 peer-checked:hover:bg-gradient-to-r peer-checked:hover:from-blue-500 peer-checked:hover:to-green-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-violet-50-dark peer-checked:after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-400 peer-checked:from-slate-400"></div> */}
-            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer bg-gradient-to-r from-blue-500 to-green-400 peer-checked:hover:bg-gradient-to-r peer-checked:hover:from-blue-500 peer-checked:hover:to-green-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-violet-50-dark peer-checked:after:bg-white after:bg-zinc-800 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-400 peer-checked:from-slate-400"></div>
+            {/* <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer bg-gradient-to-r from-[#378fe6] to-[#3eda82] peer-checked:hover:bg-gradient-to-r peer-checked:hover:from-[#378fe6] peer-checked:hover:to-[#3eda82] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[#f5f7ff]-dark peer-checked:after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#aeb3cb] peer-checked:from-[#aeb3cb]"></div> */}
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer bg-gradient-to-r from-[#378fe6] to-[#3eda82] peer-checked:hover:bg-gradient-to-r peer-checked:hover:from-[#378fe6] peer-checked:hover:to-[#3eda82] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[#f5f7ff]-dark peer-checked:after:bg-white after:bg-[#1f212e] after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#aeb3cb] peer-checked:from-[#aeb3cb]"></div>
           </label>
         </div>
 
         <div className="flex items-center justify-center h-full">
-          <div className="w-full rounded-md bg-sky-500 pt-1">
-            <div className="justify-evenly rounded-b-md h-52 bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 flex flex-col text-center">
+          <div className="w-full rounded-md bg-[#198ff5] pt-1">
+            <div className="justify-evenly rounded-b-md h-52 bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] flex flex-col text-center">
               <div className="flex justify-center items-center">
                 <img src={facebook} className="h-fit pr-2" alt="" />
                 <p>@nathanf</p>
               </div>
               <div>
-                <h1 className="text-5xl text-neutral-800 dark:text-white">
+                <h1 className="text-5xl text-[#1e202a] dark:text-white">
                   1987
                 </h1>
                 <p className="font-normal tracking-[.5em]">FOLLOWERS</p>
@@ -113,14 +113,14 @@ const DashboardComponent = () => {
         </div>
 
         <div className="flex items-center justify-center h-full">
-          <div className="w-full rounded-md bg-sky-500 pt-1">
-            <div className="justify-evenly rounded-b-md h-52 bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 flex flex-col text-center">
+          <div className="w-full rounded-md bg-[#1ca0f2] pt-1">
+            <div className="justify-evenly rounded-b-md h-52 bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] flex flex-col text-center">
               <div className="flex justify-center items-center">
                 <img src={twitter} className="h-fit pr-2" alt="" />
                 <p>@nathanf</p>
               </div>
               <div>
-                <h1 className="text-5xl text-neutral-800 dark:text-white">
+                <h1 className="text-5xl text-[#1e202a] dark:text-white">
                   1044
                 </h1>
                 <p className="font-normal tracking-[.5em]">FOLLOWERS</p>
@@ -134,14 +134,14 @@ const DashboardComponent = () => {
         </div>
 
         <div className="flex items-center justify-center h-full">
-          <div className="w-full rounded-md bg-gradient-to-r from-orange-300 to-pink-500 pt-1">
-            <div className="justify-evenly rounded-b-md h-52 bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 flex flex-col text-center">
+          <div className="w-full rounded-md bg-gradient-to-r from-[#fdc468] to-[#df4996] pt-1">
+            <div className="justify-evenly rounded-b-md h-52 bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] flex flex-col text-center">
               <div className="flex justify-center items-center">
                 <img src={instagram} className="h-fit pr-2" alt="" />
                 <p>@realnathanf</p>
               </div>
               <div>
-                <h1 className="text-5xl text-neutral-800 dark:text-white">
+                <h1 className="text-5xl text-[#1e202a] dark:text-white">
                   11k
                 </h1>
                 <p className="font-normal tracking-[.5em]">FOLLOWERS</p>
@@ -155,21 +155,21 @@ const DashboardComponent = () => {
         </div>
 
         <div className="flex items-center justify-center h-full">
-          <div className="w-full rounded-md bg-rose-700 pt-1">
-            <div className="justify-evenly rounded-b-md h-52 bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 flex flex-col text-center">
+          <div className="w-full rounded-md bg-[#c4032a] pt-1">
+            <div className="justify-evenly rounded-b-md h-52 bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] flex flex-col text-center">
               <div className="flex justify-center items-center">
                 <img src={youtube} className="h-fit pr-2" alt="" />
                 <p> Nathan F.</p>
               </div>
               <div>
-                <h1 className="text-5xl text-neutral-800 dark:text-white">
+                <h1 className="text-5xl text-[#1e202a] dark:text-white">
                   8239
                 </h1>
                 <p className="font-normal tracking-[.5em]">FOLLOWERS</p>
               </div>
               <div className="flex justify-center items-center">
                 <img className="h-fit pr-1" src={down} alt="" />
-                <p className="text-red-500">144 Today</p>
+                <p className="text-[#dc414c]">144 Today</p>
               </div>
             </div>
           </div>
@@ -177,12 +177,12 @@ const DashboardComponent = () => {
 
         <h2 className="text-2xl lg:col-span-4">Overview - Today</h2>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Page Views</p>
           <div className="flex items-center justify-end">
             <img src={facebook} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             87
           </h2>
 
@@ -192,27 +192,27 @@ const DashboardComponent = () => {
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Likes</p>
           <div className="flex items-center justify-end">
             <img src={facebook} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             52
           </h2>
 
           <div className="flex items-center justify-end">
             <img className="h-fit pr-1" src={down} alt="" />
-            <p className="text-red-500">2%</p>
+            <p className="text-[#dc414c]">2%</p>
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Likes</p>
           <div className="flex items-center justify-end">
             <img src={instagram} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             5462
           </h2>
 
@@ -222,12 +222,12 @@ const DashboardComponent = () => {
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Profile Views</p>
           <div className="flex items-center justify-end">
             <img src={instagram} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             52k
           </h2>
 
@@ -237,12 +237,12 @@ const DashboardComponent = () => {
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Retweets</p>
           <div className="flex items-center justify-end">
             <img src={twitter} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             117
           </h2>
 
@@ -252,12 +252,12 @@ const DashboardComponent = () => {
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Likes</p>
           <div className="flex items-center justify-end">
             <img src={twitter} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             507
           </h2>
 
@@ -267,33 +267,33 @@ const DashboardComponent = () => {
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Likes</p>
           <div className="flex items-center justify-end">
             <img src={youtube} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             107
           </h2>
 
           <div className="flex items-center justify-end">
             <img className="h-fit pr-1" src={down} alt="" />
-            <p className="text-red-500">19%</p>
+            <p className="text-[#dc414c]">19%</p>
           </div>
         </div>
 
-        <div className="h-32 rounded-md w-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 grid grid-cols-2 p-4 gap-1">
+        <div className="h-32 rounded-md w-full bg-[#f0f2fa] hover:bg-[#e1e3f0] dark:bg-[#252a41] dark:hover:bg-[#333a56] grid grid-cols-2 p-4 gap-1">
           <p className="flex items-center">Total Views</p>
           <div className="flex items-center justify-end">
             <img src={youtube} alt="" />
           </div>
-          <h2 className="flex items-center text-neutral-800 dark:text-white text-3xl">
+          <h2 className="flex items-center text-[#1e202a] dark:text-white text-3xl">
             1407
           </h2>
 
           <div className="flex items-center justify-end">
             <img className="h-fit pr-1" src={down} alt="" />
-            <p className="text-red-500">12%</p>
+            <p className="text-[#dc414c]">12%</p>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,46 @@ module.exports = {
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+          //   colors: {
+    //     // Primary
+    //     "lime-green": "hsl(163, 72%, 41%);", | emerald-500 X
+    //     "bright-red": "hsl(356, 69%, 56%);", | red-500 X
+
+    //     facebook: "hsl(208, 92%, 53%);", | sky-500 X
+    //     twitter: "hsl(203, 89%, 53%);", | sky-500 X
+    //     insta1: "hsl(37, 97%, 70%);", | orange-300 X
+    //     insta2: "hsl(329, 70%, 58%);", | pink-500 X
+    //     youtube: "hsl(348, 97%, 39%);", | rose-700 X
+
+    //     // Toggle
+    //     "toggle1-dark": "hsl(210, 78%, 56%);", | blue-500 X
+    //     "toggle2-dark": "hsl(146, 68%, 55%);", | green-400 X
+    //     "toggle-light": "hsl(230, 22%, 74%);", | slate-400 X
+
+    //     // Light Mode
+    //     "custom-bg": "hsl(0, 0%, 100%);", | white X
+    //     "custom-bg-top": "hsl(225, 100%, 98%);", | violet-50 X
+    //     "card-color": "hsl(227, 47%, 96%);", | slate-100 X
+    //     "card-color-active": "#e1e3f0;", | slate-200 X
+    //     "custom-text1": "hsl(228, 12%, 44%);", | gray-500 X
+    //     "custom-text2": "hsl(230, 17%, 14%);", | neutral-800 X
+
+    //     // Dark Mode
+    //     "custom-bg-dark": "hsl(230, 17%, 14%);", | neutral-800 X
+    //     "custom-bg-top-dark": "hsl(232, 19%, 15%);", | zinc-800 X
+    //     "card-color-dark": "hsl(228, 28%, 20%);", | slate-800 X
+    //     "card-color-active-dark": "#333a56;", | slate-700 X
+    //     "custom-text1-dark": "hsl(228, 34%, 66%);", | slate-400 X
+    //     "custom-text2-dark": "hsl(0, 0%, 100%);", | white X
+    //   },
+
+    // This piece of crap alongside the arbitrary values was the one stopping my build the entire time, gee thanks for informing me after i started to get genuinely heated from seeing npm build fail for the 25th time tailwind docs (oh wait you never did... jerks)
+    // Anyways, I found a link to convert all that color crap into base tailwind colors, just use the Replace All feature with CTRL+F
+
+    // Secondary Note: WTF did that website do to the colors??? I hate React now more than I did before...
+    // Third Note: Maybe I should blame Tailwind instead... 
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
